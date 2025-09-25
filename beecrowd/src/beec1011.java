da4176a784d886ced78bc22eb65bc53b7f27ac23
a1ibekovna <toromyrzaevat@icloud.com>
2025-09-25 11:32:40 +0600

    feat: Beec1011 Accepted

---
+++ beecrowd/src/beec1011.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class beec1011{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double R = sc.nextDouble();
+        double volume = (4.0/3.0) * 3.14159 * R * R * R;
+        System.out.printf("VOLUME = %.3f%n", volume);
+    }
+}
